fbb935bc27783b4be6bd53df2f7497d4bdca6912
Alex Vincent <alexv@microsoft.com>
2022-12-02 14:01:23 +0000

    dev container  updates

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,6 @@
+#!/bin/sh
+
+echo "postCreateCommand.sh"
+echo "--------------------"
+
 sudo chmod +x .devcontainer/postStartCommand.sh
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "postStartCommand.sh"
+echo "-------------------"
+
 sudo apt-get update
 sudo apt-get upgrade -y
 
