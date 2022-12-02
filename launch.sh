34c20dd8767e882dafff5a5aa1e49a378859ed57
Alex Vincent <alexv@microsoft.com>
2022-12-02 10:33:07 +0000

    tweaks

--- launch.sh
+++ launch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #set env variables during debugging
-APP_GIT_REPO="10000"
-APP_GIT_BRANCH="2"
-APP_GIT_COMMIT="3"
-APP_GIT_TAG="4"
-APP_GITHUB_WORKFLOW="5"
-APP_GITHUB_RUN_ID="6"
-APP_GITHUB_RUN_NUMBER="7"
+APP_GIT_REPO=$(basename `git rev-parse --show-toplevel`)
+APP_GIT_BRANCH=$(git branch --show-current)
+APP_GIT_COMMIT=$(git rev-parse HEAD)
+APP_GIT_TAG="n/a"
+APP_GITHUB_WORKFLOW="n/a"
+APP_GITHUB_RUN_ID=0
+APP_GITHUB_RUN_NUMBER=0
 
 RUST_LOG=debug
 
